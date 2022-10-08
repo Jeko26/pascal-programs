25accfcea1006154e6979471c1802ed8416a8d65
Evgenik <109359580+Jeko26@users.noreply.github.com>
2022-10-08 17:38:30 +0300

    Add files via upload

---
+++ shop.pas
@@ -0,0 +1,20 @@
+﻿program n;
+var a,b,c,d,sum:integer;
+begin
+  writeln('Введите кол-во булок хлеба:');
+  read(a);
+  writeln('Введите цену одной булки хлеба:');
+  read(b);
+  writeln('Введите кол-во бутылок молока:');
+  read(c);
+  writeln('Введите цену одной бутылки молока:');
+  read(d);
+  sum:=a*b+c*d;
+  write('за ');
+  write(a);
+  write('булок хлеба и ');
+  write(c);
+  write('бутылок молока вы заплатите ');
+  write(sum);
+  write(' руб');
+end.
